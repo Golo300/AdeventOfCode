787d25504c97cb1b568937bcc046033e3efd8aa6
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-12-03 23:24:03 +0100

    2024|3 complex

--- 2024/day3/main.py
+++ 2024/day3/main.py
@@ -1,5 +1,8 @@
 import re
 
+number_debug_1 = []
+number_debug_2 = []
+
 def readInput(f):
     input = ""
     for line in f:
@@ -39,8 +42,91 @@ def task2(input):
 
         if i in output_index:
             if state:
+
+                number_debug_2.append(int(output[macht_counter][0]))
+                number_debug_2.append(int(output[macht_counter][1]))
+
                 sum += int(output[macht_counter][0]) * int(output[macht_counter][1])
             macht_counter += 1
+    return sum
+
+# "\d" -> [0-9]
+# "" -> any
+automat = [
+        {"m": 1, "d": 13},# 0
+        {"u": 2},# 1
+        {"l": 3},# 2
+        {"(": 4},# 3
+
+        {"#D": 5},# 4
+        {"#D": 6, ",": 8},# 5
+        {"#D": 7, ",": 8},# 6
+        {",": 8},# 7
+
+        {"#D": 9},# 8
+        {"#D": 10, ")": 12},# 9
+        {"#D": 11, ")": 12},# 10
+        {")": 12},# 11
+        {}, # 12
+
+        {"o": 14},# 13
+        {"n": 15},# 14
+        {"'": 16},# 15
+        {"t": 17},# 16
+        {"(": 18},# 17
+        {")": 19},# 18
+
+        {"": 19, "d": 20},# 19
+        {"": 19, "o": 21},# 20
+        {"": 19, "(": 22},# 21
+        {"": 19, ")": 0},# 22
+        
+        ]
+start = 0
+accepted = [ 12 ]
+
+
+def iterateAutomat(a, start, accepted, input):
+    sum = 0
+    state = start
+    first = True
+    firstStr = ""
+    secondStr = ""
+
+    for i in input:
+        if 4 <= state <= 11:
+            if i == ",":
+                first = False
+            if i.isnumeric():
+                if first:
+                    firstStr += i
+                else:
+                    secondStr += i
+        else:
+            firstStr = ""
+            secondStr = ""
+            first = True
+
+        inputStr = i
+        if i.isnumeric():
+            inputStr = "#D"
+
+        if inputStr in a[state].keys():
+            state = a[state][inputStr]
+            if state in accepted and i == ")":
+                number_debug_1.append(int(firstStr))
+                number_debug_1.append(int(secondStr))
+                sum += int(firstStr) * int(secondStr)
+                state = 0
+        elif "" in a[state].keys():
+            state = a[state][""]
+            if state in accepted and i == ")":
+                number_debug_1.append(int(firstStr))
+                number_debug_1.append(int(secondStr))
+                sum += int(firstStr) * int(secondStr)
+                state = 0
+        else:
+            state = 0
     return sum
 
 if __name__ == "__main__":
@@ -49,3 +135,4 @@ if __name__ == "__main__":
     input = readInput(f)
     print(f"Part1: {task1(input)}")
     print(f"Part2: {task2(input)}")
+    print(f"Part2 with help: {iterateAutomat(automat, start, accepted, input)}")
